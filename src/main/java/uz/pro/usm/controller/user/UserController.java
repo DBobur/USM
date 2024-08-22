6c39f4b96642959eecae161a5bee28bfa07b3043
bobur <davlatboyev2024@gmail.com>
2024-08-22 04:51:47 -0700

    new period go on 1

--- src/main/java/uz/pro/usm/controller/user/UserController.java
+++ src/main/java/uz/pro/usm/controller/user/UserController.java
@@ -5,7 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.pro.usm.domain.dto.request.user.UserUpdateRequest;
+import uz.pro.usm.domain.dto.response.user.UserResponse;
 import uz.pro.usm.service.user.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -17,5 +20,27 @@ public class UserController {
     public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody UserUpdateRequest userUpdateRequest) {
         userService.updateUser(id, userUpdateRequest);
         return ResponseEntity.ok("User updated successfully");
+    }
+
+    @PreAuthorize("hasAnyAuthority('DELETE_USER')")
+    //@PreAuthorize("hasRole('SUPER')")
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(@RequestParam(name = "id") Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+
+    @PreAuthorize("hasAnyAuthority('GET_ALL_USERS')")
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PreAuthorize("hasAnyAuthority('GET_USER_BY_ID')")
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        UserResponse user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
     }
 }
