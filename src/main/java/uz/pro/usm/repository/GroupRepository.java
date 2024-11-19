52a154b271f6364690fab19f80dbd6209ad16290
DBobur <davlatboyev2024@gmail.com>
2024-11-19 14:39:58 +0500

    start new s

--- src/main/java/uz/pro/usm/repository/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.pro.usm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uz.pro.usm.domain.entity.Group;
-
-@Repository
-public interface GroupRepository extends JpaRepository<Group,Long> {
-}
